6633fd1bcbdae8fc1741455cc02d30e2c0189989
Deepthi <deepthi.gaddipati.f2jn@statefarm.com>
2021-08-16 15:20:38 -0500

    Added the code for the Name and email formatter task

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -4,9 +4,14 @@ public class Application {
     public static void main(String[] args) {
         //System.out.println("hello world");
         if(args.length < 1) {
-            System.out.println("Please provide an argument");
-        } else {
-            System.out.println(String.format("Arguments provided: %s %s", args[0], args[1]));
+            System.out.println("Please specify a name and email");
+       } else if (args.length == 1) {
+           //System.out.println(args[0].length());
+           System.out.println(String.format("Please specify an email for %s", (args[0])));
+        }
+           else {
+            //System.out.println(args[0].length());
+            System.out.println(String.format("%s <%s>", args[0], args[1]));
         }
     }
 }
