fb68bbe66e40b202fac726c89c95ff9c095d28bb
Deepthi <deepthi.gaddipati.f2jn@statefarm.com>
2021-08-16 13:58:44 -0500

    Code to accept arguments from the command line

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -2,6 +2,11 @@ package com.statefarm;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        //System.out.println("hello world");
+        if(args.length < 1) {
+            System.out.println("Please provide an argument");
+        } else {
+            System.out.println(String.format("Arguments provided: %s %s", args[0], args[1]));
+        }
     }
 }
